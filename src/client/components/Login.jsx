6d5e1ab1daa6b0a710e484b7f74b2235df769890
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-04-21 23:18:38 -0700

    created sign-in page

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../stylesheets/app.css';
+import SignIn from "./SignIn.js";
 
 function Login () {
   return (
     <div className="hero">
-      
+
+      <SignIn />
     </div>
   )
 }
