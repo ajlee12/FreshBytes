ef9372f08d3346eb392a21f6516e4fb1e29a9506
Jimmy Phong <jayacados@gmail.com>
2020-04-21 17:06:30 -0700

    Setup for FreshBytes collaboration project. This will be the initial code team members will fork from.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const express = require('express');
 const productControllers = require('../controllers/productControllers')
 const router = express.Router();
@@ -8,16 +9,16 @@ const router = express.Router();
 // });
 // products index
 //this route may need to change -- this is just a placeholder
-router.get('/:zip/products',productControllers.getZip,(req, res) => {
+router.get('/:zip/products', productControllers.getZip, (req, res) => {
   res.status(200).json(res.locals.products)
 });
 // product add
 // this route may need to change -- this is just a placeholder
 router.post('/products/new',
-productControllers.sellerSave,
-productControllers.productSave, // will save client info to the two tables tb, the product and the seller 
-productControllers.getZip, // will send updated zip code match to the front end 
-(req, res) => {
-  console.log('zip post');
-});
+  productControllers.sellerSave,
+  productControllers.productSave, // will save client info to the two tables tb, the product and the seller 
+  productControllers.getZip, // will send updated zip code match to the front end 
+  (req, res) => {
+    console.log('zip post');
+  });
 module.exports = router;
